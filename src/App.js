8cf064a932bcdbaaed664990b07535aa541da6d0
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2018-04-30 21:28:54 -0300

    Introduction to JSX

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Welcome to The Road to learn React';
+    const fullName = {
+      name: 'Marquinhus',
+      lastName: 'Gonçalves'
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{`${helloWorld} ${fullName.name} ${fullName.lastName}`}</h2>
       </div>
     );
   }
