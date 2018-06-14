46eb1bb37c4ab71e07d0c37184a8be6a66035ef0
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2018-06-14 14:16:44 -0300

    Add enzyme for test and plus test in table

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App, {Search, Button, Table} from './App';
+
+Enzyme.configure({adapter: new Adapter()});
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -50,7 +54,6 @@ describe('Button', () => {
 });
 
 describe('Table', () => {
-
   const props = {
     list: [
       {title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y'},
@@ -62,6 +65,13 @@ describe('Table', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Table {...props} />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table {...props} />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
   });
   test('has a valid snapshot', () => {
     const component = renderer.create(
